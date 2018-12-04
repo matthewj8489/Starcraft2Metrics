5136a57cc48564cc4308517f50512fd72831c0fc
matthew <matthewj8489@gmail.com>
2018-12-04 16:38:18 -0600

    added adept tracking - found out that adepts that are warped in do not show up in the UnitBornEvents

--- test/example.py
+++ test/example.py
@@ -33,6 +33,28 @@ for ude in unit_done_events:
 
 adept_born_events = list(filter(lambda ube: ube.unit.name == "Adept", unit_born_events))
 
+total_adepts_born = 0
 for abe in adept_born_events:
     if abe.control_pid == 1:
         print("{} created at second {}".format(abe.unit, abe.second))
+        total_adepts_born += 1
+
+
+adept_init_events = list(filter(lambda uie: uie.unit.name == "Adept", unit_init_events))
+
+total_adepts_init = 0
+for aie in adept_init_events:
+    if aie.control_pid == 1:
+        total_adepts_init += 1
+        
+
+adept_done_events = list(filter(lambda ude: ude.unit.name == "Adept", unit_done_events))
+
+total_adepts_done = 0
+for ade in adept_done_events:
+    total_adepts_done += 1
+
+
+print("Total Adepts Born ({})".format(total_adepts_born))        
+print("Total Adepts Init ({})".format(total_adepts_init))
+print("Total Adepts Done ({})".format(total_adepts_done))        
--- test/sc2replay_statistics.py
+++ test/sc2replay_statistics.py
@@ -25,7 +25,11 @@ def worker_compare(benchmark, player_workers):
 def adept_created(replay, player_id):
     adepts = []
     for event in replay.events:
+        # UnitBornEvent for gateway spawned adepts, UnitInitEvent for warped-in adepts
         if event.name == "UnitBornEvent" and event.control_pid == player_id:
+            if event.unit.name == "Adept" or event.unit.name == "adept":
+                adepts.append(event.second)
+        elif event.name == "UnitInitEvent" and event.control_pid == player_id:
             if event.unit.name == "Adept" or event.unit.name == "adept":
                 adepts.append(event.second)
 
@@ -56,6 +60,8 @@ def ROC(wp, total):
 rep_bench = sc2reader.load_replay("test_replays\\PVZ_ADEPT_BENCHMARK.SC2Replay")
 rep_test = sc2reader.load_replay("test_replays\\PVZ_ADEPT_TESTCASE1.SC2Replay")
 
+#### WORKER TRACKING ####
+
 wc_bench = worker_created(rep_bench, 1)
 wc_test = worker_created(rep_test, 1)
 
@@ -84,14 +90,14 @@ plt.savefig('bin\\worker_dev.svg')
 plt.figure()
 plt.plot(wc_roc, label='roc')
 plt.legend(loc=2)
-plt.savefig('bin\\roc.svg')
+plt.savefig('bin\\worker_roc.svg')
 
 plt.figure()
 plt.plot(wc, label='worker time')
 plt.plot(w_dev, label='worker deviation')
 plt.plot(wc_roc, label='roc')
 plt.legend(loc=2)
-plt.savefig('bin\\combined.svg')
+plt.savefig('bin\\worker_combined.svg')
 
 plt.figure()
 yb = []
@@ -105,3 +111,57 @@ for y in range(len(wc_test)):
 plt.plot(wc_test, yp, label='player workers')
 plt.legend(loc=2)
 plt.savefig('bin\\worker_created.svg')
+
+
+#### ADEPT TRACKING ####
+
+ac_bench = adept_created(rep_bench, 1)
+ac_test = adept_created(rep_test, 1)
+
+ac = worker_compare(ac_bench, ac_test)
+
+#sum up the deviation at each point that an adept is created
+a_dev = []
+a_sum = 0
+for x in range(len(ac)):
+    a_sum += ac[x]
+    a_dev.append(a_sum)
+
+#ac_roc = ROC(ac, len(ac))
+ac_roc = ROC(a_dev, len(a_dev))
+
+plt.figure()
+plt.plot(ac, label='adept time')
+plt.legend(loc=2)
+plt.savefig('bin\\adept_time.svg')
+
+plt.figure()
+plt.plot(a_dev, label='adept deviation')
+plt.legend(loc=2)
+plt.savefig('bin\\adept_dev.svg')
+
+plt.figure()
+plt.plot(ac_roc, label='roc')
+plt.legend(loc=2)
+plt.savefig('bin\\adept_roc.svg')
+
+plt.figure()
+plt.plot(ac, label='adept time')
+plt.plot(a_dev, label='adept deviation')
+plt.plot(ac_roc, label='roc')
+plt.legend(loc=2)
+plt.savefig('bin\\adept_combined.svg')
+
+plt.figure()
+yb = []
+for y in range(len(ac_bench)):
+    yb.append(y)
+plt.plot(ac_bench, yb, label='benchmark adepts')
+
+yp = []
+for y in range(len(ac_test)):
+    yp.append(y)
+plt.plot(ac_test, yp, label='player adepts')
+plt.legend(loc=2)
+plt.savefig('bin\\adept_created.svg')
+

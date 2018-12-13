a0e9ca3d3f9981c0181c3629f9e595d79c13420f
matthew <matthewj8489@gmail.com>
2018-12-13 16:43:01 -0600

    filling out more of the bo deviation function

--- test/build_order_deviation.py
+++ test/build_order_deviation.py
@@ -1,7 +1,7 @@
 import sc2reader
 
 
-def build_order_deviation(bench_replay_location, player_replay_location):
+def build_order_deviation(bench_replay_location, player_replay_location, player_name):
     rep_bench = sc2reader.load_replay(bench_replay_location)
     rep_player = sc2reader.load_replay(player_replay_location)
 
@@ -11,13 +11,38 @@ def build_order_deviation(bench_replay_location, player_replay_location):
     min_game_length = min(b_game_length, p_game_length)
 
     # create a dictionary of events
-    event_names = set([event.name for event in replay.events])
-    events_of_type = (name: [] for name in event_names)
-    unit_born_events = events_of_type["UnitBornEvent"]
-    unit_init_events = events_of_type["UnitInitEvent"]
+    bench_event_names = set([event.name for event in rep_bench.events])
+    bench_events_of_type = (name: [] for name in event_names)
+    bench_unit_born_events = events_of_type["UnitBornEvent"]
+    bench_unit_init_events = events_of_type["UnitInitEvent"]
+
+    rep_event_names = set([event.name for event in rep_player.events])
+    rep_events_of_type = (name: [] for name in event_names)
+    rep_unit_born_events = events_of_type["UnitBornEvent"]
+    rep_unit_init_events = events_of_type["UnitInitEvent"]
+
+    bench_units_born = list(filter(lambda evt: evt.control_pid == bench_pid, bench_unit_born_events))
+    bench_units_init = list(filter(lambda evt: evt.control_pid == bench_pid, bench_unit_init_events))
+    replay_units_born = list(filter(lambda evt: evt.control_pid == rep_pid, rep_unit_born_events))
+    replay_units_init = list(filter(lambda evt: evt.control_pid == rep_pid, rep_unit_init_events))    
+
+    # determine the pid of the player testing the BO deviation
+    bench_pid = 0
+    if rep_bench.players[0].name == player_name:
+        bench_pid = rep_bench.players[0].pid
+    else:
+        bench_pid = rep_bench.players[1].pid
+
+    rep_pid = 0
+    if rep_player.players[0].name == player_name:
+        rep_pid = rep_player.players[0].pid
+    else:
+        rep_pid = rep_player.players[1].pid   
 
     # for each relevant unit (a unit that was created), calculate a BOD and DEV
 
+
+    
 
   
 #### PRIVATE FUNCTIONS ####

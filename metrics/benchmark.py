df2db44ecaeedfa4e7970c84382c2551c4a76e48
matthew <matthewj8489@gmail.com>
2019-02-21 16:55:29 -0600

    creating a benchmark class to use instead of individual functions.
    creating a conversion from gametime to realtime in util

--- metrics/benchmark.py
+++ metrics/benchmark.py
@@ -1,6 +1,90 @@
 import sc2reader
 import util
 
+class Benchmark(object):
+    def __init__(self, replay_file, player_id):
+        #: Replay structure
+        self._replay = sc2reader.load_replay(replay_file)
+
+        #: An events dictionary from the replay
+        event_names = set([event.name for event in self._replay.events])
+        self._events = {name: [] for name in event_names}
+        for event in self._replay.events:
+            self._events[event.name].append(event)
+         
+
+        #: The ID of the player for whom to parse benchmark data
+        self._player_id = player_id
+
+
+    def workers_created(self, real_time_s):
+        if not 'UnitBornEvent' in self._events:
+            return 0
+        unit_born_events = self._events['UnitBornEvent']
+        worker_count = 0
+        game_time_s = util.convert_realtime_to_gametime_r(self._replay, real_time_s)
+
+        for ube in unit_born_events:
+            if ube.control_pid == self._player_id:
+                if ube.unit.is_worker and ube.second <= game_time_s:
+                    worker_count += ube.unit.supply
+
+        return worker_count
+
+
+    def army_created(self, real_time_s):
+        if not 'UnitBornEvent' in self._events or not 'UnitInitEvent' in self._events:
+            return 0
+        unit_born_events = self._events['UnitBornEvent']
+        unit_init_events = self._events['UnitInitEvent']   
+        army_supply_count = 0
+        game_time_s = util.convert_realtime_to_gametime_r(self._replay, real_time_s)
+
+        player_ube = list(filter(lambda ube: ube.control_pid == self._player_id, unit_born_events))
+        player_army_ube = list(filter(lambda ube: ube.unit.is_army, player_ube))
+
+        for ube in player_army_ube:
+            if (ube.second <= game_time_s) and (ube.unit.name != "Archon") and (not ube.unit.hallucinated):
+                army_supply_count += ube.unit.supply
+
+        player_uie = list(filter(lambda uie: uie.control_pid == self._player_id, unit_init_events))
+        player_army_uie = list(filter(lambda uie: uie.unit.is_army, player_uie))
+
+        for uie in player_army_uie:
+            if (uie.second <= game_time_s) and (uie.unit.name != "Archon") and (not uie.unit.hallucinated):
+                army_supply_count += uie.unit.supply
+
+        return army_supply_count
+
+
+    def total_supply_created(self, real_time_s):
+        supply = 0
+        supply += self.workers_created(real_time_s)
+        supply += self.army_created(real_time_s)
+
+        return supply
+
+
+    def time_to_66_workers(self):
+        if not 'UnitBornEvent' in self._events:
+            return 0
+
+        unit_born_events = self._events['UnitBornEvent']
+        worker_count = 0
+
+        for ube in unit_born_events:
+            if (ube.control_pid == self._player_id) and (ube.unit.is_worker):
+                worker_count += 1
+                if worker_count >= 66:
+                    return util.convert_gametime_to_realtime_r(self._replay, ube.second)
+
+        return -1
+
+
+
+
+
+
 def benchmark(replay_file, player_id, benchmark_time_s):
     '''
     Find benchmark metrics for the given replay file at the given real time.
@@ -39,8 +123,8 @@ def benchmark(replay_file, player_id, benchmark_time_s):
 
     game_bench_time_s = util.convert_realtime_to_gametime_r(replay, benchmark_time_s)
 
-    workers = worker_created_benchmark(events_dictionary, player_id, game_bench_time_s)
-    army = army_created_benchmark(events_dictionary, player_id, game_bench_time_s)
+    workers = workers_created(events_dictionary, player_id, game_bench_time_s)
+    army = army_created(events_dictionary, player_id, game_bench_time_s)
     total_supply_created = workers + army
     replay_time_measured = min(benchmark_time_s, replay.game_length.seconds)
     
@@ -49,7 +133,7 @@ def benchmark(replay_file, player_id, benchmark_time_s):
 
 
 ########## PRIVATE ###########
-def worker_created_benchmark(event_dict, player_id, benchmark_time_s):
+def workers_created(event_dict, player_id, game_time_s):
     if not 'UnitBornEvent' in event_dict:
         return 0
     unit_born_events = event_dict['UnitBornEvent']
@@ -57,13 +141,13 @@ def worker_created_benchmark(event_dict, player_id, benchmark_time_s):
 
     for ube in unit_born_events:
         if ube.control_pid == player_id:
-            if ube.unit.is_worker and ube.second <= benchmark_time_s:
+            if ube.unit.is_worker and ube.second <= game_time_s:
                 worker_count += ube.unit.supply
 
     return worker_count
 
 
-def army_created_benchmark(event_dict, player_id, benchmark_time_s):
+def army_created(event_dict, player_id, game_time_s):
     if not 'UnitBornEvent' in event_dict or not 'UnitInitEvent' in event_dict:
         return 0
     unit_born_events = event_dict['UnitBornEvent']
@@ -74,39 +158,34 @@ def army_created_benchmark(event_dict, player_id, benchmark_time_s):
     player_army_ube = list(filter(lambda ube: ube.unit.is_army, player_ube))
 
     for ube in player_army_ube:
-        if (ube.second <= benchmark_time_s) and (ube.unit.name != "Archon") and (not ube.unit.hallucinated):
+        if (ube.second <= game_time_s) and (ube.unit.name != "Archon") and (not ube.unit.hallucinated):
             army_supply_count += ube.unit.supply
 
     player_uie = list(filter(lambda uie: uie.control_pid == player_id, unit_init_events))
     player_army_uie = list(filter(lambda uie: uie.unit.is_army, player_uie))
 
     for uie in player_army_uie:
-        if (uie.second <= benchmark_time_s) and (uie.unit.name != "Archon") and (not uie.unit.hallucinated):
+        if (uie.second <= game_time_s) and (uie.unit.name != "Archon") and (not uie.unit.hallucinated):
             army_supply_count += uie.unit.supply
 
     return army_supply_count
 
-def supply_available_benchmark(event_dict, player_id, benchmark_time_s):
-    supply_count = 0
 
-    return supply_count
+def time_to_66_workers(event_dict, player_id):
+    if not 'UnitBornEvent' in event_dict:
+        return 0
 
-def bases_completed_benchmark(event_dict, player_id, benchmark_time_s):
-    bases_completed = 0
+    unit_born_events = event_dict['UnitBornEvent']
+    worker_count = 0
 
-    return bases_completed
+    for ube in unit_born_events:
+        if (ube.control_pid == player_id) and (ube.unit.is_worker):
+            worker_count += 1
+            if workert_count >= 66:
+                return ube.second
 
-def minerals_mined_benchmark(event_dict, player_id, benchmark_time_s):
-    minerals_mined = 0
 
-    return minerals_mined
-
-def gas_mined_benchmark(event_dict, player_id, benchmark_time_s):
-    gas_mined = 0
-
-    return gas_mined
-
-def units_created(event_dict, player_id, benchmark_time_s):
+def units_created(event_dict, player_id, game_time_s):
     if not 'UnitBornEvent' in event_dict or not 'UnitInitEvent' in event_dict:
         return 0
     units_created = {"Probe": 0, "Zealot": 0, "Sentry": 0, "Stalker": 0, "Adept": 0, "HighTemplar": 0,
@@ -120,11 +199,11 @@ def units_created(event_dict, player_id, benchmark_time_s):
     player_uie = list(filter(lambda uie: uie.control_pid == player_id, unit_init_events))
 
     for ube in player_ube:
-        if (ube.second <= benchmark_time_s) and (ube.unit.name in units_created) and (ube.unit.hallucinated == False):
+        if (ube.second <= game_time_s) and (ube.unit.name in units_created) and (ube.unit.hallucinated == False):
             units_created[ube.unit.name] += 1
 
     for uie in player_uie:
-        if (uie.second <= benchmark_time_s) and (uie.unit.name in units_created) and (uie.unit.hallucinated == False):
+        if (uie.second <= game_time_s) and (uie.unit.name in units_created) and (uie.unit.hallucinated == False):
             units_created[uie.unit.name] += 1
 
     return units_created
@@ -141,8 +220,8 @@ if __name__ == '__main__':
 
     bench_time_s = util.convert_realtime_to_gametime_r(replay, 618)
 
-    workers = worker_created_benchmark(events_dictionary, 1, bench_time_s)
-    army = army_created_benchmark(events_dictionary, 1, bench_time_s)
+    workers = worker_created(events_dictionary, 1, bench_time_s)
+    army = army_created(events_dictionary, 1, bench_time_s)
     unit_dict = units_created(events_dictionary, 1, bench_time_s)
 
     bench_dict = benchmark("..\\test\\test_replays\\Year Zero LE (9).SC2Replay", 1, 618)
--- metrics/util.py
+++ metrics/util.py
@@ -19,3 +19,16 @@ def convert_realtime_to_gametime_r(replay, real_time_s):
     real_time_s -- the real time in the replay in seconds.
     '''
     return convert_realtime_to_gametime(real_time_s, replay.frames, replay.game_fps, replay.game_length.seconds)
+
+
+def convert_gametime_to_realtime(game_time_s, game_frames, game_fps, game_length_s):
+    real_time = 0
+
+    real_game_frame = game_time_s * game_fps
+    real_fps = game_frames / game_length_s
+    real_time = real_game_frame // real_fps
+
+    return real_time
+
+def convert_gametime_to_realtime_r(replay, game_time_s):
+    return convert_gametime_to_realtime(game_time_s, replay.frames, replay.game_fps, replay.game_length.seconds)

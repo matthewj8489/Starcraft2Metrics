a7ff9dcc401cf059e2a4afb0fd2abda99705e9e8
matthew <matthewj8489@gmail.com>
2019-03-01 17:16:34 -0600

    filled out the command a little more

--- replay_renamer.bat
+++ replay_renamer.bat
@@ -1,4 +1,10 @@
 python sc2autosave.py 							\
 	--source ~/My\ Documents/Starcraft\ II/Accounts/.../Multiplayer \
-	--dest ~/My\ Documents/Starcraft2Replays			\
+	--dest ~/My\ Documents/Starcraft2Replays/1v1Ladder		\
+	--rename "{:date}({:length}) {:matchup} on {:map}: {:teams}"	\
+	--player-format "{:name}({:play_race})"				\
+	--date-format "YYYY-MM-DD"					\
+	--filter-matchup 1v1						\
+
+	
 	

d642506951f7edef52cdff311f970a653d0f791c
Matthew <matthewj8489@gmail.com>
2019-03-18 20:22:16 -0500

    adding a batch file to test out the replay parser

---
+++ test/rep_parser.bat
@@ -0,0 +1,1 @@
+python ..\metrics\replay_parser.py "C:\Users\matthew\Documents\Starcraft2Metrics\test\test_replays" NULL --overwrite --ladderonly --gametype 1v1 --outpath "C:\Users\matthew\Documents\Starcraft2Metrics\test\bin" 

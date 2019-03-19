3d3a7a99828ca14cfa5fe302d4ed4174bccc891e
matthew <matthewj8489@gmail.com>
2019-03-19 14:00:37 -0500

    added path relatives to make the script run independent of directory location

--- test/rep_parser.bat
+++ test/rep_parser.bat
@@ -1,1 +1,3 @@
-python ..\metrics\replay_parser.py "C:\Users\matthew\Documents\Starcraft2Metrics\test\test_replays" NULL --overwrite --ladderonly --gametype 1v1 --outpath "C:\Users\matthew\Documents\Starcraft2Metrics\test\bin" 
+REM python ..\metrics\replay_parser.py "C:\Users\matthew\Documents\Starcraft2Metrics\test\test_replays" NULL --overwrite --ladderonly --gametype 1v1 --outpath "C:\Users\matthew\Documents\Starcraft2Metrics\test\bin" 
+REM python ..\metrics\replay_parser.py "test_replays" NULL --overwrite --ladderonly --gametype 1v1 --outpath "bin" 
+python ..\metrics\replay_parser.py "test_replays" NULL --ladderonly --gametype 1v1 --outpath "bin" 

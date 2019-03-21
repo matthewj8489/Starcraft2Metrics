b6270afc2c1422b1f39a46b617dc6d7a3cba4c17
matthew <matthewj8489@gmail.com>
2019-03-21 17:06:31 -0500

    testing out some plugins

--- test/example.py
+++ test/example.py
@@ -2,11 +2,16 @@ import sc2reader
 from sc2reader.engine.plugins import APMTracker#, SelectionTracker, ContextLoader, SupplyTracker
 import supply
 from supply import SupplyTracker
+from workers_created import WorkersCreatedTracker
+from army_created import ArmyCreatedTracker
+from bases_created import BasesCreatedTracker
 #sc2reader.engine.register_plugin(APMTracker())
 #sc2reader.engine.register_plugin(SelectionTracker())
 #sc2reader.engine.register_plugin(ContextLoader())
 sc2reader.engine.register_plugin(SupplyTracker())
-
+sc2reader.engine.register_plugin(WorkersCreatedTracker())
+sc2reader.engine.register_plugin(ArmyCreatedTracker())
+sc2reader.engine.register_plugin(BasesCreatedTracker())
 
 #replay = sc2reader.load_replay("test_replays\\PVZ_ADEPT_BENCHMARK.SC2Replay")
 replay = sc2reader.load_replay("test_replays\\Year Zero LE (9).SC2Replay")

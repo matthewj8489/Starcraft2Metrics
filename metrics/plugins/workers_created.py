07bdeeffc8476fc618f233691b2a3647cac5d0a5
matthew <matthewj8489@gmail.com>
2019-03-21 16:41:53 -0500

    adding a worker created counter plugin

---
+++ metrics/plugins/workers_created.py
@@ -0,0 +1,22 @@
+class Worker(object):
+
+    def __init__(self, second):
+        self.second = second
+
+    
+class WorkersCreatedTracker(object):
+
+    name = 'WorkersTracker'
+
+
+    def handleInitGame(self,event,replay):
+        for player in replay.players:
+            player.workers_created = []
+
+
+    def handleUnitBornEvent(self,event,replay):
+        if event.unit.is_worker:
+            replay.player[event.unit.owner.pid].workers_created.append(
+                Worker(event.second))
+            
+            

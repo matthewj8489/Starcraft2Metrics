4b9d763ba28e32cffb5f427b120ac0aa5d371818
Matthew <matthewj8489@gmail.com>
2019-03-22 17:50:24 -0500

    forgot to add the init files

---
+++ metrics/__init__.py
@@ -0,0 +1,5 @@
+##import sc2reader
+##from sc2reader.engine.plugins import APMTracker
+##from supply import SupplyTracker
+##sc2reader.engine.register_plugin(APMTracker())
+##sc2reader.engine.register_plugin(SupplyTracker())

52b0023df6fd790d57e5c4f84a8660e7e02f7c5f
matthew <matthewj8489@gmail.com>
2019-03-25 10:34:53 -0500

    adding the necessary plugin registering

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,5 +1,10 @@
-##import sc2reader
-##from sc2reader.engine.plugins import APMTracker
-##from supply import SupplyTracker
-##sc2reader.engine.register_plugin(APMTracker())
-##sc2reader.engine.register_plugin(SupplyTracker())
+import sc2reader
+from plugins.supply import SupplyTracker
+from plugins.bases_created import BasesCreatedTracker
+from plugins.supply_created import SupplyCreatedTracker
+from plugins.resources import ResourceTracker
+
+sc2reader.engine.register_plugin(SupplyTracker())
+sc2reader.engine.register_plugin(BasesCreatedTracker())
+sc2reader.engine.register_plugin(SupplyCreatedTracker())
+sc2reader.engine.register_plugin(ResourceTracker())
--- metrics/test.py
+++ metrics/test.py
@@ -1,14 +1,10 @@
 import sc2reader
 from plugins.supply import SupplyTracker
-#from plugins.workers_created import WorkersCreatedTracker
-#from plugins.army_created import ArmyCreatedTracker
 from plugins.bases_created import BasesCreatedTracker
 from plugins.supply_created import SupplyCreatedTracker
 from plugins.resources import ResourceTracker
 
 sc2reader.engine.register_plugin(SupplyTracker())
-#sc2reader.engine.register_plugin(WorkersCreatedTracker())
-#sc2reader.engine.register_plugin(ArmyCreatedTracker())
 sc2reader.engine.register_plugin(BasesCreatedTracker())
 sc2reader.engine.register_plugin(SupplyCreatedTracker())
 sc2reader.engine.register_plugin(ResourceTracker())

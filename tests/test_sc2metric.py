b898d441baadf4a811b1ecf2c151eedf42c3ae6b
matthew <matthewj8489@gmail.com>
2019-03-28 15:22:43 -0500

    adding some more unit tests

--- tests/test_sc2metric.py
+++ tests/test_sc2metric.py
@@ -45,7 +45,15 @@ class TestMetrics(unittest.TestCase):
         
         self.assertEqual(p1_met.time_to_supply_created(200), 618)
         self.assertEqual(p1_met.time_to_supply_created_max_workers(200, 75), 618)
-        
+
+
+    def test_time_to_X_workers(self):
+        replay = sc2reader.load_replay("test_replays\\standard_1v1.SC2Replay")
+        p1_met = replay.player[1].metrics
+
+        self.assertEqual(p1_met.time_to_workers_created(66), 500)
+        self.assertEqual(p1_met.time_to_workers_created(75), 612)
+
 
 ##    def test_time_to_bases(self):
 ##        replay = sc2reader.load_replay("")

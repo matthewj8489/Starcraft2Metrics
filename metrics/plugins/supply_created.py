7bde294d46ada9aa28d5fcb03f2add9d21e45430
Matthew <matthewj8489@gmail.com>
2019-04-02 21:02:00 -0500

    fixing how supply was tracked for workers and army

--- metrics/plugins/supply_created.py
+++ metrics/plugins/supply_created.py
@@ -11,8 +11,28 @@ class SupplyCreatedTracker(object):
 
     def __init__(self):
         self._supply_created = defaultdict(int)
+        self._workers_created = defaultdict(int)
+        self._army_created = defaultdict(int)
 
 
+    def _add_to_workers(self,event,replay):
+        self._workers_created[event.unit.owner.pid] += event.unit.supply
+        supp = SupplyCount(util.convert_gametime_to_realtime_r(replay,event.second),
+                           self._workers_created[event.unit.owner.pid],
+                           event.unit.supply,
+                           event.unit.is_worker)
+        replay.player[event.unit.owner.pid].metrics.workers_created.append(supp)
+        
+        
+    def _add_to_army(self,event,replay):
+        self._army_created[event.unit.owner.pid] += event.unit.supply
+        supp = SupplyCount(util.convert_gametime_to_realtime_r(replay,event.second),
+                           self._army_created[event.unit.owner.pid],
+                           event.unit.supply,
+                           event.unit.is_worker)
+        replay.player[event.unit.owner.pid].metrics.army_created.append(supp)
+        
+        
     def _add_to_supply(self,event,replay):
         self._supply_created[event.unit.owner.pid] += event.unit.supply
         supp = SupplyCount(util.convert_gametime_to_realtime_r(replay,event.second),
@@ -20,25 +40,28 @@ class SupplyCreatedTracker(object):
                            event.unit.supply,
                            event.unit.is_worker)
         replay.player[event.unit.owner.pid].metrics.supply_created.append(supp)
-        if event.unit.is_worker:
-            replay.player[event.unit.owner.pid].metrics.workers_created.append(supp)
-        elif event.unit.is_army:
-            replay.player[event.unit.owner.pid].metrics.army_created.append(supp)
 
 
     def handleInitGame(self,event,replay):
         for player in replay.players:
             player.metrics = Sc2MetricAnalyzer()
             self._supply_created[player.pid] = 0
+            self._workers_created[player.pid] = 0
+            self._army_created[player.pid] = 0
 
 
     def handleUnitBornEvent(self,event,replay):
-        if event.unit.is_worker or (event.unit.is_army
-                                    and not util.is_hallucinated(event.unit)
-                                    and event.unit.name != "Archon"):
+        if event.unit.is_worker:
             self._add_to_supply(event,replay)
-
+            self._add_to_workers(event,replay)
+        elif (event.unit.is_army
+              and not util.is_hallucinated(event.unit)
+              and event.unit.name != "Archon"):
+            self._add_to_supply(event,replay)
+            self._add_to_army(event,replay)
+            
 
     def handleUnitInitEvent(self,event,replay):
         if (event.unit.is_army and not util.is_hallucinated(event.unit) and event.unit.name != "Archon"):
             self._add_to_supply(event,replay)
+            self._add_to_army(event,replay)

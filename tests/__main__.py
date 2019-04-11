d16fddfbd2e76b6117cb8a55a5a0a3243aba6a21
Matthew <matthewj8489@gmail.com>
2019-04-10 19:03:10 -0500

    another attempt at getting these paths correct

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,8 +1,17 @@
 import os
 import sys
 
+#if __name__ == '__main__':
+#    sys.path.insert(0, os.path.abspath("..\\"))
+
+import inspect
+
 if __name__ == '__main__':
-    sys.path.insert(0, os.path.abspath("..\\"))
+    filename = os.path.abspath(inspect.getfile(inspect.currentframe()))
+    thispath = os.path.dirname(filename)
+    normpath = os.path.normpath(os.path.join(thispath, os.pardir))
+    sys.path.insert(0, normpath)
+
     
 if sys.version_info[:2] < (2, 7):
     import unittest2 as unittest

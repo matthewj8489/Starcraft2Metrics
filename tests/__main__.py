1944a772a3dcee71b0f4610868470a1e9495e272
matthew <matthewj8489@gmail.com>
2019-04-16 15:50:47 -0500

    refining the tests to focus on one unit each

--- tests/__main__.py
+++ tests/__main__.py
@@ -19,12 +19,12 @@ else:
     import unittest
     
 import metrics    
-import test_plugins
+#import test_plugins
 import test_sc2metric
-import test_bases_created_plugin
-from test_plugins import TestPlugins
+#import test_bases_created_plugin
+#from test_plugins import TestPlugins
 from test_sc2metric import TestMetrics
-from test_bases_created_plugin import TestBasesCreatedPlugin
+#from test_bases_created_plugin import TestBasesCreatedPlugin
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_sc2metric.py
+++ tests/test_sc2metric.py
@@ -57,6 +57,12 @@ class TestMetrics(unittest.TestCase):
         p1_met = replay.player[1].metrics
                                 
         self.assertEqual(p1_met.time_to_supply_created(200), 612) # for some reason i see 613 at earliest in replay
+
+
+    def test_time_to_max_with_max_workers_created(self):
+        replay = sc2reader.load_replay(os.path.join(REPLAY_DIR, "standard_1v1.SC2Replay"))
+        p1_met = replay.player[1].metrics
+        
         self.assertEqual(p1_met.time_to_supply_created_max_workers(200, 75), 612) # for some reason i see 613 at earliest in replay
 
 
@@ -123,31 +129,56 @@ class TestMetrics(unittest.TestCase):
         p1_met = replay.player[1].metrics
         
         self.assertEqual(round(p1_met.avg_sq()), 105)
+
+
+    def test_avg_sq_at_time(self):
+        ## TODO
         #self.assertEqual(round(p1_met.avg_sq_at_time(500), 1), 110.5)
+        pass
+
+
+    def test_avg_sq_pre_max(self):
+        ## TODO
         #self.assertEqual(round(p1_met.avg_sq_pre_max(), 1), 122.8)
+        pass
             
             
-    def test_aur(self):
-        #replay = sc2reader.load_replay("test_replays\\sc62_aur559.SC2Replay")
-        #p1_met = replay.player[1].metrics
-        
-        #self.assertEqual(round(p1_met.aur()), 559)
-        #self.assertEqual(round(p1_met.aur_at_time(500)), ???)
-        #self.assertEqual(round(p1_met.aur_pre_max()), ???)
-        
+    def test_aur(self):       
         replay = sc2reader.load_replay(os.path.join(REPLAY_DIR, "pvt_macro1.SC2Replay"))
         p1_met = replay.player[1].metrics
         
         self.assertEqual(round(p1_met.aur()), 1428)
-        
+
+
+    def test_aur_at_time(self):
+        ## TODO
+        #self.assertEqual(round(p1_met.aur_at_time(500)), ???)
+        pass
+
+
+    def test_aur_pre_max(self):
+        ## TODO
+        #self.assertEqual(round(p1_met.aur_pre_max()), ???)
+        pass
+    
         
     def test_avg_rcr(self):
         replay = sc2reader.load_replay(os.path.join(REPLAY_DIR, "pvt_macro1.SC2Replay"))
         p1_met = replay.player[1].metrics
         
         self.assertEqual(round(p1_met.avg_rcr()), 2491)
+
+
+    def test_avg_rcr_at_time(self):
+        ## TODO
         #self.assertEqual(round(p1_met.avg_rcr_at_time(500)), ???)
+        pass
+
+
+    def test_avg_rcr_pre_max(self):
+        ## TODO
         #self.assertEqual(round(p1_met.avg_rcr_pre_max()), ???)
+        pass
             
             
     def test_apm(self):

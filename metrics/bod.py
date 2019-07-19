1880fa572eee255f6ade6feba338423c73b09af1
matthew <matthewj8489@gmail.com>
2019-07-19 14:44:39 -0500

    adding more tests adding some plots of data to bod

--- metrics/bod.py
+++ metrics/bod.py
@@ -1,3 +1,4 @@
+import itertools
 
 # Outputs:
 # dev - a metric that weights supply differences, time differences, order
@@ -21,6 +22,11 @@
 #           (6)[1:50]21|Assim,(6)[1:62]22|Assim,+1,+0:12
 #           (7)[2:00]22|Probe,(8)[2:15]22|Probe,0,+0:15
 #           (8)[2:10]22|Pylon,(7)[2:00]22|Pylon,0,-0:10
+#
+#
+# Additions:
+#   - Add a parameter to calculate deviations that defines how far into the
+#       bench bo to go. (i.e. to the nth build)
 
 class BuildOrderDeviation(object):
 
@@ -35,23 +41,27 @@ class BuildOrderDeviation(object):
         self.order_dev = 0
         self.discrepency = 0
         self.worst_dev = None
+        self.acc_time_dev = []
         self.dev_arr = []
 
-    def calculate_deviations(self, compare_bo):
+    def calculate_deviations(self, compare_bo, n=-1):
         self._initialize()
         
-        cmp_bo = self._get_sorted_build_order(compare_bo)
+        cmp_bo = self._get_sorted_build_order(compare_bo, n)
         #shortest_bo = len(self._bench_bo) if len(self._bench_bo) < len(compare_bo) else len(compare_bo)
 
-        for idx in range(len(self._bench_bo)):
+        for idx in range(n if n >= 0 else len(self._bench_bo)):
             if cmp_bo[idx] is not None:
                 self.supp_dev += abs(self._bench_bo[idx].supply - cmp_bo[idx].supply)
                 self.time_dev += abs(self._bench_bo[idx].time - cmp_bo[idx].time)
+                self.acc_time_dev.append(self.time_dev)
                 print("BCH:{0} - CMP:{1} - sp:{2} - tm:{3}".format(self._bench_bo[idx].to_string(),
                                                                    cmp_bo[idx].to_string(),
                                                                    cmp_bo[idx].supply - self._bench_bo[idx].supply,
                                                                    cmp_bo[idx].time - self._bench_bo[idx].time))
             else:
+                self.time_dev += abs(self._bench_bo[idx].time - self._bench_bo[-1].time)
+                self.acc_time_dev.append(self.time_dev)
                 print("BCH:{0} - None".format(self._bench_bo[idx].to_string()))
 
 
@@ -60,8 +70,9 @@ class BuildOrderDeviation(object):
             if x.name == nm:
                 yield x
 
-    def _get_sorted_build_order(self, bo):
+    def _get_sorted_build_order(self, bo, n=-1):
         sort_bo = []
+        last_build_num = n if n >= -1 else self._bench_bo[-1].build_num
 
         # create a dictionary of iterators, each iterator returns the next
         # element that matches the build unit name
@@ -79,10 +90,20 @@ class BuildOrderDeviation(object):
             tmp_bo = next(iters_of_type[boe.name], None)
             # if the build order difference is greater than 20, then discount
             # this element as a correct part of the build
-            if tmp_bo is not None and abs(tmp_bo.build_num - boe.build_num) <= 20:
+##            if tmp_bo is not None and abs(tmp_bo.build_num - boe.build_num) <= 20:
+##                sort_bo.append(tmp_bo)
+##            else:
+##                # need to push back the last bo element into the iterator...
+##                sort_bo.append(None)
+
+            # if the build unit order is greater than the last build unit
+            # order of the bench, or if the build order difference is greater
+            # than 20, then the element is not a correct part of the build.
+            if tmp_bo is not None and tmp_bo.build_num <= last_build_num + 20 and abs(tmp_bo.build_num - boe.build_num) <= 20:
                 sort_bo.append(tmp_bo)
             else:
                 sort_bo.append(None)
+                iters_of_type[boe.name] = itertools.chain([tmp_bo], iters_of_type[boe.name])
 
         return sort_bo
         
@@ -106,11 +127,34 @@ if __name__ == '__main__':
 
     bod = BuildOrderDeviation(boe_bench)
 
-    bod.calculate_deviations(boe_exec)
+    bod.calculate_deviations(boe_exec, n=63)
 
     print(bod.supp_dev)
     print(bod.time_dev)
+
+    import matplotlib.pyplot as plt
+
+    plt.plot(bod.acc_time_dev)
+
+    plt.xlabel('build order')
+    plt.ylabel('time dev (s)')
+    plt.title('Accumulated Time Deviation')
+    plt.grid(True)
+    plt.savefig('acc_time_dev.png')
+    plt.show()
     
+    roc = []
+    for d in range(1, len(bod.acc_time_dev)):
+        roc.append((bod.acc_time_dev[d] - bod.acc_time_dev[d-1]))
+
+    plt.plot(roc)
+    plt.xlabel('build order')
+    plt.ylabel('ROC of time dev (s/bo)')
+    plt.ylim(top=30)
+    plt.title('ROC of Accumulated Time Deviation')
+    plt.savefig('roc_acc_time_dev.png')
+    plt.show()
+        
     
     
     
--- tests/unit/test_bod.py
+++ tests/unit/test_bod.py
@@ -15,6 +15,8 @@ from metrics.metric_containers import *
 
 class TestBuildOrderDeviation(unittest.TestCase):
 
+############################ SUPPLY DEVIATION TESTS ############################
+    
     def test_supply_deviation_calculated_correctly_when_late_on_supply(self):
         golden_bo = []
         compare_bo = []
@@ -94,7 +96,7 @@ class TestBuildOrderDeviation(unittest.TestCase):
         self.assertEqual(bo_dev.supp_dev, 0)        
 
 
-    def test_time_deviation_calculated_correctly_when_build_is_missing_element(self):
+    def test_supply_deviation_calculated_correctly_when_build_is_missing_element(self):
         golden_bo = []
         compare_bo = []
         golden_bo.append(BuildOrderElement(1, 'Probe', 12, 0, 0))
@@ -109,6 +111,8 @@ class TestBuildOrderDeviation(unittest.TestCase):
 
         self.assertEqual(bo_dev.supp_dev, 0)        
         
+
+############################ TIME DEVIATION TESTS ############################
 
     def test_time_deviation_calculated_correctly_when_late_on_time(self):
         golden_bo = []
@@ -184,7 +188,7 @@ class TestBuildOrderDeviation(unittest.TestCase):
         bo_dev = BuildOrderDeviation(golden_bo)
         bo_dev.calculate_deviations(compare_bo)
 
-        self.assertEqual(bo_dev.time_dev, 0)
+        self.assertEqual(bo_dev.time_dev, 30)
 
 
     def test_time_deviation_isnt_affected_when_build_order_deviates_by_more_than_20(self):
@@ -195,11 +199,48 @@ class TestBuildOrderDeviation(unittest.TestCase):
         golden_bo.append(BuildOrderElement(3, 'Assimilator', 15, 50, 200))
 
         compare_bo.append(BuildOrderElement(1, 'Probe', 12, 0, 0))
-        compare_bo.append(BuildOrderElement(3, 'Assimilator', 15, 50, 200))
-        compare_bo.append(BuildOrderElement(23, 'Pylon', 14, 20, 90))
+        compare_bo.append(BuildOrderElement(23, 'Pylon', 14, 25, 90))
+        compare_bo.append(BuildOrderElement(24, 'Assimilator', 15, 50, 200))
 
         bo_dev = BuildOrderDeviation(golden_bo)
         bo_dev.calculate_deviations(compare_bo)
+
+        self.assertEqual(bo_dev.time_dev, 30)
+        
+
+    def test_time_deviation_isnt_affected_when_build_order_deviates_by_more_than_20_beyond_last_build_unit(self):
+        golden_bo = []
+        compare_bo = []
+        golden_bo.append(BuildOrderElement(1, 'Probe', 12, 0, 0))
+        golden_bo.append(BuildOrderElement(2, 'Pylon', 14, 20, 90))
+        golden_bo.append(BuildOrderElement(3, 'Assimilator', 15, 50, 200))
+
+        compare_bo.append(BuildOrderElement(1, 'Probe', 12, 0, 0))
+        compare_bo.append(BuildOrderElement(3, 'Assimilator', 15, 50, 200))
+        compare_bo.append(BuildOrderElement(24, 'Pylon', 14, 20, 90))
+
+        bo_dev = BuildOrderDeviation(golden_bo)
+        bo_dev.calculate_deviations(compare_bo)
+
+        self.assertEqual(bo_dev.time_dev, 30)
+
+
+############################ TEST Nth BUILD NUM LIMITOR ############################
+
+    def test_build_order_comparison_is_limited_when_setting_build_num_limitor(self):
+        golden_bo = []
+        compare_bo = []
+        golden_bo.append(BuildOrderElement(1, 'Probe', 12, 0, 0))
+        golden_bo.append(BuildOrderElement(2, 'Pylon', 14, 20, 90))
+        golden_bo.append(BuildOrderElement(3, 'Assimilator', 15, 50, 200))
+
+        compare_bo.append(BuildOrderElement(1, 'Probe', 12, 0, 0))
+        compare_bo.append(BuildOrderElement(2, 'Pylon', 14, 20, 90))
+        compare_bo.append(BuildOrderElement(3, 'Assimilator', 16, 51, 201))
+        
+
+        bo_dev = BuildOrderDeviation(golden_bo)
+        bo_dev.calculate_deviations(compare_bo, n=2)
 
         self.assertEqual(bo_dev.time_dev, 0)
 

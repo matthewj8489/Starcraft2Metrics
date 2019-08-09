ec475e61b89b5bb387f4b1a3dd1497e825e1c900
matthew <matthewj8489@gmail.com>
2019-08-09 10:02:57 -0500

    adding a sc2reader factory

---
+++ metrics/metric_factory/sc2reader_factory.py
@@ -0,0 +1,23 @@
+import sc2reader
+
+from metric_containers import *
+from metrics.util import convert_to_realtime_r
+
+class Sc2ReaderFactory(object):
+
+    def __init__(self, file_path):
+        self._replay = sc2reader.load_replay(file_path)
+
+    def generateResourcesTracked(self, player_name):
+        res = []
+        
+        for plyr in self._replay.players:
+            if plyr.name == player_name:
+                plyr_stats_events = list(filter(lambda evts: evts.name == 'PlayerStatsEvent', plyr.events))
+                for evt in plyr_stats_events:
+                    res.append(ResourceCount(convert_to_realtime_r(self._replay, evt.second),
+                                             (evt.minerals_collection_rate + evt.vespene_collection_rate),
+                                             (evt.minerals_current + evt.vespene_current)))
+                break
+
+        return res
